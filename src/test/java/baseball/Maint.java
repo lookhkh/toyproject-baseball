66e6f583d24fa222669d0092cdd0cad666acadb7
Juneseo <lookhkh37@gmail.com>
2023-05-27 14:55:59 +0900

    first commit

---
+++ src/test/java/baseball/Maint.java
@@ -0,0 +1,13 @@
+package baseball;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+public class Maint {
+
+	@Test
+	public void test() {
+		assertEquals(1, 1);
+	}
+}
